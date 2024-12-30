73ff8111af071168fdf23d51f2ca26660de85e4f
hauchenglee <hauchenglee@gmail.com>
2024-12-30 22:29:31 +0800

    1. food, add, add, favorite 基本layout完成

---
+++ pages/food/all/all.vue
@@ -0,0 +1,14 @@
+<template>
+    <view class="div"></view>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.div {
+    height: 100px;
+    background: red;
+}
+</style>
--- pages/food/food.vue
+++ pages/food/food.vue
@@ -1,32 +1,85 @@
 <template>
-
+    <view class="page-container">
+        <view class="layout-banner">
+            <view class="banner__content">
+            </view>
+        </view>
+        <view class="layout-segmented">
+            <fui-segmented-control :values="values2" @click="itemClick"></fui-segmented-control>
+        </view>
+        <view class="layout-segmented-detail">
+            <component :is="currentComponent" v-if="currentComponent"></component>
+        </view>
+    </view>
 </template>
 
-<script setup>
-import {onShow} from '@dcloudio/uni-app'
-import {ref} from 'vue'
+<script>
+// 引入需要动态加载的子页面
+import AddPage from "@/pages/food/add/add.vue";
+import AllPage from "@/pages/food/all/all.vue";
+import FavoritePage from "@/pages/food/favorite/favorite.vue";
 
-// 登录检查
-onShow(() => {
-    const isLoggedIn = uni.getStorageSync('isLoggedIn')
-    if (!isLoggedIn) {
-        uni.reLaunch({
-            url: '/pages/login/login'
-        })
+export default {
+    components: {
+        AddPage,
+        AllPage,
+        FavoritePage
+    },
+    data() {
+        return {
+            values2: [
+                {name: '新增', component: 'AddPage'},
+                {name: '全部', component: 'AllPage'},
+                {name: '收藏', component: 'FavoritePage'}
+            ],
+            currentComponent: null // 当前显示的组件
+        };
+    },
+    methods: {
+        itemClick(e) {
+            // 获取点击的组件名称
+            const selectedComponent = this.values2[e.index].component;
+            console.log("Switching to component:", selectedComponent);
+
+            // 设置当前显示的组件
+            this.currentComponent = selectedComponent;
+        }
     }
-})
-
-// Segmented 数据
-const values = ref(['segmented1', 'segmented2'])
-
-// Segmented 点击方法
-const itemClick = (e) => {
-    console.log(e)
-}
+};
 </script>
 
 <style>
-.food-base {
-    padding: 20rpx;
+.page-container {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+}
+
+.layout-banner {
+    height: 100px; /* 固定高度 */
+    background-color: #fff;
+    position: relative;
+    z-index: 1;
+}
+
+.layout-segmented {
+    height: 50px; /* 固定高度 */
+    background-color: #fff;
+    position: relative;
+    z-index: 1;
+}
+
+.layout-segmented-detail {
+    flex: 1;
+    position: relative;
+    height: calc(100vh - 150px); /* 减去banner和segmented的总高度 */
+    overflow-y: auto;
+    background-color: #f5f5f5;
+}
+
+/* 确保子组件在detail区域内正确显示 */
+.layout-segmented-detail > * {
+    width: 100%;
+    height: 100%;
 }
 </style>
